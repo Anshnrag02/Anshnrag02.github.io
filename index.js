42ddd34c89626846fd3ba58742dcbb2bbe493670
Ansh Anurag <anxhnurag@gmail.com>
2021-07-28 17:05:35 +0530

    search working

--- index.js
+++ index.js
@@ -5,19 +5,27 @@ const fetchData = async (searchquery) => {
             s: searchquery,
         }
     });
+    if(response.data.Error){
+        return [];    
+    }
     return response.data.Search;
 }
 
 const input = document.querySelector('input');
 
 const oninput = async Event => {
+    document.querySelector('#target').innerHTML="";
     const movies = await fetchData(Event.target.value);
     console.log(movies);
     for(let movie of movies){
         const div = document.createElement('div');
         div.innerHTML = `
-            <img src="${movie.Poster}"/>
-            <h1>${movie.Title}</h1>
+            <a class="dropdown-item">
+                
+                <img src="${movie.Poster}"/>
+                <h1>${movie.Title}</h1>
+            </a>
+            <hr class="dropdown-divider">
         `;
 
         document.querySelector('#target').appendChild(div);
