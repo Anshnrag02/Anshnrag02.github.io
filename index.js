75a89703777b274a200e4ea407fabfc4442e8045
Ansh Anurag <anxhnurag@gmail.com>
2021-07-28 14:39:29 +0530

    render movies

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ const input = document.querySelector('input');
 const oninput = async Event => {
     const movies = await fetchData(Event.target.value);
     console.log(movies);
+    for(let movie of movies){
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <img src="${movie.Poster}"/>
+            <h1>${movie.Title}</h1>
+        `;
+
+        document.querySelector('#target').appendChild(div);
+
+    }
 }
 
 input.addEventListener('input', debounce(oninput,1000));
